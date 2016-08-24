0e8beeea4e61f1ee5e17a9cad89fa5f9654f139d
rrigato <rrigato@horizon.csueastbay.edu>
2016-08-23 17:13:44 -0700

    I have all the data from 2010 until 2015

--- mergeData.py
+++ mergeData.py
@@ -17,11 +17,19 @@ class quarterBack:
 		self.twenty15 = pd.read_csv("C:/Users/Punkiehome1/Downloads/qbResearch/qb2015.csv")
 		self.twenty14 = pd.read_csv("C:/Users/Punkiehome1/Downloads/qbResearch/qb2014.csv")
 		self.twenty13 = pd.read_csv("C:/Users/Punkiehome1/Downloads/qbResearch/qb2013.csv")
-		self.twenty12 = pd.read_csv("C:/Users/Punkiehome1/Downloads/qbResearch/qb2013.csv")
-		print("World")
+		self.twenty12 = pd.read_csv("C:/Users/Punkiehome1/Downloads/qbResearch/qb2012.csv")
+		self.twenty11 = pd.read_csv("C:/Users/Punkiehome1/Downloads/qbResearch/qb2011.csv")
+		self.twenty10 = pd.read_csv("C:/Users/Punkiehome1/Downloads/qbResearch/qb2010.csv")
 		
 	def changeName(self):
+		print(self.twenty15.shape)
+		print(self.twenty14.shape)
+		print(self.twenty13.shape)
+		print(self.twenty12.shape)
+		print(self.twenty11.shape)
+		print(self.twenty10.shape)
 
 if __name__ == "__main__":
 	qbObj = quarterBack()
+	qbObj.changeName()
 	
